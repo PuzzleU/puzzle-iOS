ec1a0ccfeb5f2dcf38b465243ba04ec53ada192a
LeeMyeongJin <dlaudwls1203@gmail.com>
2024-01-25 21:56:58 +0900

    [Setting] #3 - Initial project setup

---
+++ Puzzle-iOS/Puzzle-iOS/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Puzzle-iOS
+//
+//  Created by 이명진 on 1/25/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

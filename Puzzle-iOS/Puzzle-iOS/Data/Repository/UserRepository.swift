4232ab68ebb600da03cd72a518bde5bab6c4617e
shinzz410 <tlswldnjs410@gmail.com>
2024-02-01 17:38:16 +0900

    [Setting] #8 - set network of social login

---
+++ Puzzle-iOS/Puzzle-iOS/Data/Repository/UserRepository.swift
@@ -0,0 +1,39 @@
+//
+//  UserRepository.swift
+//  Puzzle-iOS
+//
+//  Created by 신지원 on 2/1/24.
+//
+
+import Foundation
+import Combine
+
+protocol UserRepository {
+    func getAppleAuthToken() -> Future<String, Error>
+//    func requestAppleLogin(_ accessToken: String) ->
+    func getKakaoAuthToken() -> Future<String, Error>
+//    func requestKakaoLogin(_ accessToken: String) ->
+}
+
+final class DefaultUserRepository: UserRepository {
+    private let authAppleService: AuthAppleService
+    private let authKakaoService: AuthKakaoService
+    
+    init(
+        authAppleService: AuthAppleService,
+        authKakaoService: AuthKakaoService
+    ) {
+        self.authAppleService = authAppleService
+        self.authKakaoService = authKakaoService
+    }
+    
+    func getKakaoAuthToken() -> Future<String, Error> {
+        <#code#>
+    }
+    
+    func getAppleAuthToken() -> Future<String, Error> {
+        <#code#>
+    }
+    
+}
+
---
+++ Puzzle-iOS/Puzzle-iOS/Network/Service/AuthService/AuthKakaoService.swift
@@ -0,0 +1,20 @@
+//
+//  SocialLoginKakaoService.swift
+//  Puzzle-iOS
+//
+//  Created by 신지원 on 2/1/24.
+//
+
+import Foundation
+import Combine
+
+protocol AuthKakaoService {
+    func login() -> AnyPublisher<String, Error>
+} 
+
+final class DefaultAuthKakaoService: AuthKakaoService {
+    func login() -> AnyPublisher<String, Error> {
+        <#code#>
+    }
+    
+}

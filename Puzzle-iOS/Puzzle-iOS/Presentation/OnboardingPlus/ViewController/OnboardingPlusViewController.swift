95cdf88018f9d1e985b41b518c12e239dd4818f3
LeeMyeongJin <dlaudwls1203@gmail.com>
2024-02-10 00:15:40 +0900

    [Del] #21 - delete test code

--- Puzzle-iOS/Puzzle-iOS/Presentation/OnboardingPlus/ViewController/OnboardingPlusViewController.swift
+++ Puzzle-iOS/Puzzle-iOS/Presentation/OnboardingPlus/ViewController/OnboardingPlusViewController.swift
@@ -8,11 +8,10 @@
 import UIKit
 
 final class OnboardingPlusViewController: UIViewController {
-
+    
     // MARK: - Properties
-
+    
     private let rootView = OnboardingPlusView()
-    private lazy var naviBar = PuzzleNavigationBar(self, type: .centerTitle).setTitle("중앙 타이틀 올시다")
     
     // MARK: - Life Cycles
     
@@ -22,12 +21,7 @@ final class OnboardingPlusViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.addSubview(naviBar)
         // Do any additional setup after loading the view.
-        naviBar.snp.makeConstraints {
-            $0.top.leading.trailing.equalTo(self.view.safeAreaLayoutGuide)
-            $0.height.equalTo(40)
-        }
     }
     
 }

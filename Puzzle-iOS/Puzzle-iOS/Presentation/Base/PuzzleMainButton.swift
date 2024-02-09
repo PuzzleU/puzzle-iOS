c395da6fe92768dd8a39c2126e333d17d89e66ee
shinzz410 <tlswldnjs410@gmail.com>
2024-02-09 20:55:06 +0900

    [Design] #22 - implement custom button

--- Puzzle-iOS/Puzzle-iOS/Presentation/Base/PuzzleMainButton.swift
+++ Puzzle-iOS/Puzzle-iOS/Presentation/Base/PuzzleMainButton.swift
@@ -5,4 +5,70 @@
 //  Created by 신지원 on 2/9/24.
 //
 
-import Foundation
+import UIKit
+
+import SnapKit
+import Then
+
+final class PuzzleMainButton: UIButton {
+    
+    //MARK: - Life Cycle
+    
+    override var isSelected: Bool {
+            didSet {
+                updateButtonUI()
+            }
+        }
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+    }
+    
+    init(title: String) {
+        super.init(frame: .zero)
+        
+        setUI(title: title)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    override func didMoveToSuperview() {
+        super.didMoveToSuperview()
+        
+        setLayout()
+    }
+    
+    //MARK: - UI methods
+    
+    private func setUI(title: String) {
+        setTitle(title, for: .normal)
+        titleLabel?.font = .systemFont(ofSize: 14)
+        setTitleColor(.puzzleRealWhite, for: .normal)
+        backgroundColor = .puzzleLightGray
+        layer.cornerRadius = 15
+        addTarget(self, action: #selector(selectedButton), for: .touchUpInside)
+    }
+    
+    private func setLayout() {
+        guard let superview = superview else { return }
+        self.snp.makeConstraints() {
+            $0.centerX.equalToSuperview()
+            $0.leading.equalToSuperview().inset(22)
+            $0.height.equalTo(52)
+            $0.bottom.equalTo(superview.safeAreaLayoutGuide.snp.bottom).offset(-17)
+        }
+    }
+    
+    //MARK: - Custom methods
+    
+    @objc 
+    private func selectedButton() {
+        isSelected.toggle()
+    }
+    
+    private func updateButtonUI() {
+        backgroundColor = isSelected ? .puzzlePurple : .puzzleLightGray
+    }
+}

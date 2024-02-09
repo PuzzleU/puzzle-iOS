c73f73c29afc076a2b9141b37f3716d69ed905df
shinzz410 <tlswldnjs410@gmail.com>
2024-02-09 21:01:15 +0900

    [Design] #22 - implement onboardingPlus view and vc for test

---
+++ Puzzle-iOS/Puzzle-iOS/Presentation/OnboardingPlus/View/OnboardingPlusView.swift
@@ -0,0 +1,25 @@
+//
+//  OnboardingPlusView.swift
+//  Puzzle-iOS
+//
+//  Created by 신지원 on 2/9/24.
+//
+
+import UIKit
+
+final class OnboardingPlusView: UIView {
+
+    // MARK: - UI Components
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        self.backgroundColor = .puzzleRealWhite
+
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
+}

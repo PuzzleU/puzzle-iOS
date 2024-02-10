d6df53723db6dfbdb828245321bb94f81e45a935
LeeMyeongJin <dlaudwls1203@gmail.com>
2024-02-10 20:17:39 +0900

    [Fix] #main - add semicolon

--- Puzzle-iOS/Puzzle-iOS/Resource/Extension/UIViewController+.swift
+++ Puzzle-iOS/Puzzle-iOS/Resource/Extension/UIViewController+.swift
@@ -13,7 +13,7 @@ extension UIViewController {
         let viewController = ViewController()
         navigationController?.pushViewController(viewController, animated: true)
     }
-  
+    
     /// 뷰 컨트롤러가 네비게이션 스택에 있으면 pop 하고, 나머지는  dismiss 합니다.
     func popOrDismissViewController(animated: Bool = true) {
         if let navigationController = self.navigationController, navigationController.viewControllers.contains(self) {
@@ -25,7 +25,7 @@ extension UIViewController {
     
     func showToast(message: String, heightOffset: CGFloat? = nil) {
         let height = heightOffset ?? 233 // default 값
-
+        
         Toast.show(
             message: message,
             view: self.view,
@@ -36,5 +36,6 @@ extension UIViewController {
     
     private func safeAreaBottomInset() -> CGFloat {
         return view.safeAreaInsets.bottom
-      
+        
+    }
 }

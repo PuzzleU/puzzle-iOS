e50b9881e2d7a058104468ead4cbae314836509a
shinzz410 <tlswldnjs410@gmail.com>
2024-02-14 21:34:19 +0900

    [Add] #32 - add UITableViewCell extension

---
+++ Puzzle-iOS/Puzzle-iOS/Resource/UIComponents/PuzzleDropDownTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  PuzzleDropDownTableViewCell.swift
+//  Puzzle-iOS
+//
+//  Created by 신지원 on 2/14/24.
+//
+
+import UIKit
+
+class PuzzleDropDownTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}

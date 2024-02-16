67078941ca78cba8711efea2baf7831f90fbd7ce
LeeMyeongJin <dlaudwls1203@gmail.com>
2024-02-16 23:43:36 +0900

    [Feat] #29 - create OnboardingSignUpNameVC

---
+++ Puzzle-iOS/Puzzle-iOS/Presentation/Onboarding/ViewController/OnboardingSignUpNameVC.swift
@@ -0,0 +1,28 @@
+//
+//  OnboardingSignUpNameVC.swift
+//  Puzzle-iOS
+//
+//  Created by 이명진 on 2/15/24.
+//
+
+import UIKit
+
+class OnboardingSignUpNameVC: UIViewController {
+    
+    private let rootView = OnboardingBaseView()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+        // Do any additional setup after loading the view.
+    }
+    
+    
+    // MARK: - Life Cycles
+    
+    override func loadView() {
+        self.view = rootView
+    }
+    
+}

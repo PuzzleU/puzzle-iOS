ad1311646ac8812ad2f4bbf041a2586b188c1a97
shinzz410 <tlswldnjs410@gmail.com>
2024-02-17 15:54:39 +0900

    [Del] #32 - delete test code

--- Puzzle-iOS/Puzzle-iOS/Presentation/ViewController.swift
+++ Puzzle-iOS/Puzzle-iOS/Presentation/ViewController.swift
@@ -11,29 +11,12 @@ import SnapKit
 
 class ViewController: UIViewController {
 
-    let dummyData = ["강아지", "토끼", "이구아나", "뱀파이어", "드래곤볼", "딱따구리딱딱딱", "돼지꿀", "고양이", "새"]
-    private let rootView = PuzzleDropdownView(title: "지워닝")
-
     // MARK: - Life Cycles
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        //Hierarchy 와 Layout 설정
-        self.view.addSubview(rootView)
-        
-        rootView.snp.makeConstraints {
-            $0.top.equalToSuperview().inset(100)
-            $0.centerX.equalToSuperview()
-            $0.height.equalTo(50)
-            $0.width.equalTo(100)
         }
 
-        DropdownManager.shared.createDropdown(mainView: rootView,
-                                              viewController: self,
-                                              dropboxSize: CGSize(width: 150, height: 150),
-                                              dropdownAlign: .leading,
-                                              dropboxData: dummyData)
-    }
 }
 

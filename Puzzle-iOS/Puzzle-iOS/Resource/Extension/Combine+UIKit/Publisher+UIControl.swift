18af04de20300a11df6906f10e039f98d4b985bc
LeeMyeongJin <dlaudwls1203@gmail.com>
2024-02-19 00:57:30 +0900

    [Feat] #29 - add Publisher+UIGesture

--- Puzzle-iOS/Puzzle-iOS/Resource/Extension/Combine+UIKit/Publisher+UIControl.swift
+++ Puzzle-iOS/Puzzle-iOS/Resource/Extension/Combine+UIKit/Publisher+UIControl.swift
@@ -21,44 +21,43 @@ extension CombineCompatible where Self: UIControl {
 final class UIControlSubscription<SubscriberType: Subscriber, Control: UIControl>: Subscription where SubscriberType.Input == Control {
     private var subscriber: SubscriberType?
     private let control: Control
-
+    
     init(subscriber: SubscriberType, control: Control, event: UIControl.Event) {
         self.subscriber = subscriber
         self.control = control
         control.addTarget(self, action: #selector(eventHandler), for: event)
     }
-
+    
     func request(_ demand: Subscribers.Demand) {
         
     }
-
+    
     func cancel() {
         subscriber = nil
     }
-
+    
     @objc private func eventHandler() {
         _ = subscriber?.receive(control)
     }
 }
 
 struct UIControlPublisher<Control: UIControl>: Publisher {
-
+    
     public typealias Output = Control
     public typealias Failure = Never
-
+    
     let control: Control
     let controlEvents: UIControl.Event
-
+    
     init(control: Control, events: UIControl.Event) {
         self.control = control
         self.controlEvents = events
     }
     
     func receive<S>(subscriber: S) where S: Subscriber,
-                                                S.Failure == UIControlPublisher.Failure,
-                                                S.Input == UIControlPublisher.Output {
-        let subscription = UIControlSubscription(subscriber: subscriber, control: control, event: controlEvents)
-        subscriber.receive(subscription: subscription)
-    }
+                                         S.Failure == UIControlPublisher.Failure,
+                                         S.Input == UIControlPublisher.Output {
+                                             let subscription = UIControlSubscription(subscriber: subscriber, control: control, event: controlEvents)
+                                             subscriber.receive(subscription: subscription)
+                                         }
 }
-

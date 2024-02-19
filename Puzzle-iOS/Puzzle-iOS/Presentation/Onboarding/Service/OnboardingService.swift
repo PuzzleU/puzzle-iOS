9fdbdc6e710c48cc3ad4b5788d80c80dce7c2072
LeeMyeongJin <dlaudwls1203@gmail.com>
2024-02-19 23:30:35 +0900

    [Feat] #29 - add OnboardingService

---
+++ Puzzle-iOS/Puzzle-iOS/Presentation/Onboarding/Service/OnboardingService.swift
@@ -0,0 +1,25 @@
+//
+//  File.swift
+//  Puzzle-iOS
+//
+//  Created by 이명진 on 2/19/24.
+//
+
+protocol OnboardingServiceType {
+    func getAnimalImage() -> AnyPublisher<UIImage, Error>
+}
+
+import UIKit
+
+import Combine
+
+class OnboardingService: OnboardingServiceType {
+    func getAnimalImage() -> AnyPublisher<UIImage, Error> {
+        return ["imgMouse", "imgFox", "imgDog", "imgTiger", "imgOx", "imgLion", "imgPanda", "imgMonkey", "imgRabbit", "imgBear", "imgCat", "imgRobot"]
+            .publisher
+            .catch { error in
+                return Fail(error: error).eraseToAnyPublisher()
+            }.compactMap { UIImage(named: $0) }
+            .eraseToAnyPublisher()
+    }
+}

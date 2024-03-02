d3fc87bf75f55824b82a824e5cf330b596e4e557
LeeMyeongJin <dlaudwls1203@gmail.com>
2024-03-02 18:26:38 +0900

    [Feat] #29 - add LabelFactory Function
    it is Reusable

---
+++ Puzzle-iOS/Puzzle-iOS/Resource/Utils/LabelFactory.swift
@@ -0,0 +1,26 @@
+//
+//  LabelFactory.swift
+//  Puzzle-iOS
+//
+//  Created by 이명진 on 3/2/24.
+//
+
+import UIKit
+import Then
+
+struct LabelFactory {
+    static func build(
+        text: String?,
+        font: UIFont,
+        backgroundColor: UIColor = .clear,
+        textColor: UIColor = .puzzleBlack,
+        textAlignment: NSTextAlignment = .center) -> UILabel {
+            return UILabel().then {
+                $0.text = text
+                $0.font = font
+                $0.backgroundColor = backgroundColor
+                $0.textColor = textColor
+                $0.textAlignment = textAlignment
+            }
+        }
+}

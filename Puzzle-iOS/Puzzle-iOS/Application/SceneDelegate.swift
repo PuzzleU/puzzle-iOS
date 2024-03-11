c63ecfc743cdf6b07d0fcabd33686c0634c3a028
LeeMyeongJin <dlaudwls1203@gmail.com>
2024-03-11 14:05:36 +0900

    [Fix] #main - change naming

--- Puzzle-iOS/Puzzle-iOS/Application/SceneDelegate.swift
+++ Puzzle-iOS/Puzzle-iOS/Application/SceneDelegate.swift
@@ -18,8 +18,8 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         guard let windowScene = (scene as? UIWindowScene) else { return }
         
         let window = UIWindow(windowScene: windowScene)
-        let nav = UINavigationController(rootViewController: ViewController())
-        window.rootViewController = nav
+        let navigationViewController = UINavigationController(rootViewController: ViewController())
+        window.rootViewController = navigationViewController
         self.window = window
         window.makeKeyAndVisible()
     }

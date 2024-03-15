a35552660a7f5272f4fbeb09f358790b27e1f98f
shinzz410 <tlswldnjs410@gmail.com>
2024-03-15 19:24:45 +0900

    [Add] #45 - connect BASE URL

--- Puzzle-iOS/Puzzle-iOS/Presentation/ViewController.swift
+++ Puzzle-iOS/Puzzle-iOS/Presentation/ViewController.swift
@@ -16,6 +16,8 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         self.view.backgroundColor = .puzzleWhite
+        
+        print(Config.baseURL)
     }
 }
 
--- Puzzle-iOS/Puzzle-iOS/Resource/Config/Config.swift
+++ Puzzle-iOS/Puzzle-iOS/Resource/Config/Config.swift
@@ -8,5 +8,6 @@
 import Foundation
 
 enum Config {
+    static let baseURL = Bundle.main.infoDictionary?["BASE_URL"] as! String
     static let kakaoNativeAppKey = Bundle.main.infoDictionary?["KAKAO_NATIVE_APPKEY"] as! String
 }

bef07ec158b79b9487c1db7c48dc479e4434b1c9
LeeMyeongJin <dlaudwls1203@gmail.com>
2024-04-15 21:13:02 +0900

    [Network] #52 - add patch server func

--- Puzzle-iOS/Puzzle-iOS/Network/Service/SplashService/SplashService.swift
+++ Puzzle-iOS/Puzzle-iOS/Network/Service/SplashService/SplashService.swift
@@ -10,6 +10,7 @@ import Combine
 
 protocol SplashService {
     func getOnboardingData() -> AnyPublisher<SplashDTO, Error>
+    func requestOnboardingUserInfo(data: UserInfoDTO) ->  AnyPublisher<Void, Error>
 }
 
 final class OnboardingService: NSObject, Networking, SplashService {
@@ -19,17 +20,38 @@ final class OnboardingService: NSObject, Networking, SplashService {
                                               method: .get,
                                               headers: APIConstants.noTokenHeader,
                                               body: nil)
-
+            
             NetworkLogger.log(request: request)
-
+            
             return URLSession.shared.dataTaskPublisher(for: request)
-                        .map(\.data)
-                        .decode(type: SplashDTO.self, decoder: JSONDecoder())
-                        .mapError { $0 as Error }
-                        .eraseToAnyPublisher()
-
+                .map(\.data)
+                .decode(type: SplashDTO.self, decoder: JSONDecoder())
+                .mapError { $0 as Error }
+                .eraseToAnyPublisher()
+            
         } catch {
             return Fail(error: error).eraseToAnyPublisher()
         }
     }
+    
+    func requestOnboardingUserInfo(data: UserInfoDTO) -> AnyPublisher<Void, Error> {
+        do {
+            let body = try JSONEncoder().encode(data)
+            let request = try makeHTTPRequest(path: URLs.Login.essential,
+                                              method: .patch,
+                                              headers: APIConstants.noTokenHeader,
+                                              body: body)
+            
+            NetworkLogger.log(request: request)
+            
+            return URLSession.shared.dataTaskPublisher(for: request)
+                .map { _ in () }
+                .mapError { $0 as Error }
+                .eraseToAnyPublisher()
+            
+        } catch {
+            return Fail(error: error).eraseToAnyPublisher()
+        }
+    }
+    
 }
